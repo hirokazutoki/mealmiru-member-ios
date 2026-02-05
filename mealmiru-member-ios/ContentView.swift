75ac8fee2155c3a7d8b540e812d7a44ef0bef178
hirokazutoki <hrkztk1124@gmail.com>
2026-02-05 21:50:09 +0900

    Initial Commit

---
+++ mealmiru-member-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  mealmiru-member-ios
+//
+//  Created by Hirokazu Toki on 2026/02/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ mealmiru-member-ios/mealmiru_member_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  mealmiru_member_iosApp.swift
+//  mealmiru-member-ios
+//
+//  Created by Hirokazu Toki on 2026/02/05.
+//
+
+import SwiftUI
+
+@main
+struct mealmiru_member_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
